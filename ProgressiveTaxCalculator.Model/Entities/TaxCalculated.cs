13c42f01642e6ac73488a5a3cd1c66eff79a3e93
Goldin Baloyi <GoldinB@hollywoodbets.net>
2023-11-22 22:32:18 +0200

    added database entities

---
+++ ProgressiveTaxCalculator.Model/Entities/TaxCalculated.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgressiveTaxCalculator.Model.Entities
+{
+    public class TaxCalculated
+    {
+        public int? Id { get; set; }
+        public int? TaxTableId { get; set; }
+        public int? TaxTypeId { get; set; }
+        public int? PostalCodeId { get; set; }
+        public int? TaxTermId { get; set; }
+        public decimal? GrossAmount { get; set; }
+        public decimal? TaxPercentage { get; set; }
+        public decimal? NettAmount { get; set; }
+        public DateTime? DateAdded { get; set; }
+
+    }
+}
