659c7966c64e421437473d059e5716be86baca9d
Goldin Baloyi <GoldinB@hollywoodbets.net>
2023-12-12 08:32:57 +0200

    added db relationships and fixed ui

--- ProgressiveTaxCalculator.Model/Entities/PostalCode.cs
+++ ProgressiveTaxCalculator.Model/Entities/PostalCode.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,7 @@ namespace ProgressiveTaxCalculator.Model.Entities
     {
         [Key]
         public int? Id { get; set; }
+        [ForeignKey(nameof(TaxType.Id))]
         public int? TaxTypeId { get; set; }
         public string? Code { get; set; }
         public bool? Active { get; set; }
